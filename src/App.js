7e5ad65da35de8c8ce9203611c79bfb115c321c6
Mitchell <mitchell@Mitchells-MacBook-Pro.local>
2022-12-27 11:01:54 -0800

    images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import b1 from './b1.jpg';
+import b2 from './b2.jpg';
+import b3 from './b3.jpg';
+import b4 from './b4.jpg';
+import b5 from './b5.jpg';
+import b6 from './b6.jpg';
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.words = ["foobar", "turing", "robot", "machine", "wintermute"]
     this.guessedLetters = new Set();
+
     // TODO does alphabet need to be bound? I would prefer a constant
     this.alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    this.images = {1: b1, 2: b2, 3: b3, 4: b4, 5: b5, 6: b6}
 
     // Initializing the state 
     this.state = { currentWord: "",
@@ -15,11 +23,11 @@ class App extends React.Component {
       gameOver: false,
       gameWon: false,
       unGuessedLetters: new Set(),
+      currentImage: this.images[0],
     };
   }
   componentDidMount() {
     let currentWordIndex = Math.floor(Math.random() * (this.words.length));
-    console.log(currentWordIndex)
     let currentWord = this.words[currentWordIndex];
 
     // get characters in currentWord
@@ -27,37 +35,42 @@ class App extends React.Component {
     for (let i = 0; i < currentWord.length; i++) {
       unGuessedLetters.add(currentWord[i]);
     }
-    console.log(currentWord)
-
     this.setState({currentWord, unGuessedLetters});
   }
   guess(letter) {
     letter = letter.toLowerCase()
-    // letter has already been guesses or
+    // letter has already been guessed or is malformed
     if (this.guessedLetters.has(letter) || letter.length !== 1 ) {
       return
     }
     this.guessedLetters.add(letter)
-    if (this.state.wrongGuesses >= 4) {
+    if (this.state.unGuessedLetters.has(letter)) {
+      this.correctGuess(letter)
+    } else {
+      this.wrongGuess()
+    }
+  }
+
+  wrongGuess() {
+    console.log("WRONG  GUESS")
+    console.log(this.state.currentImage)
+    let currentGuesses = this.state.wrongGuesses
+    this.setState({ wrongGuesses: currentGuesses + 1});
+    this.setState({currentImage: this.images[this.state.numWrongGuesses + 1]})
+
+    if (this.state.wrongGuesses >= 6) {
       this.setState({gameOver: true})
     }
-    // wrong guess
-    if (!this.state.unGuessedLetters.has(letter)) {
-      let currentGuesses = this.state.wrongGuesses
-      this.setState({ wrongGuesses: currentGuesses + 1});
-    } else {
-      // correct guess
-      console.log(this.state.unGuessedLetters)
-      let unGuessedLetters = this.state.unGuessedLetters
-      unGuessedLetters.delete(letter)
-      this.setState({unGuessedLetters})
-      if (unGuessedLetters.size === 0) {
-        this.setState({gameWon: true})
-      }
-      console.log(this.state.unGuessedLetters)
+  }
+  correctGuess(letter) {
+    console.log("correct  GUESS")
 
+    let unGuessedLetters = this.state.unGuessedLetters
+    unGuessedLetters.delete(letter)
+    this.setState({unGuessedLetters})
+    if (unGuessedLetters.size === 0) {
+      this.setState({gameWon: true})
     }
-
   }
   render() {
     return (
@@ -72,10 +85,11 @@ class App extends React.Component {
               <p>game won</p>
           }
         </div>
+        <img src={this.state.currentImage} alt="Logo" />
         <p>{this.state.currentWord}</p>
+        <p>wrong guesses: {this.state.wrongGuesses}</p>
         <div className="alphabet">
         {this.alphabet.map(character => (
-          // <div className="letter">{character}</div>
           <div className="letter" onClick={() => this.guess(character)}>{character}</div>
         ))}
         </div>
